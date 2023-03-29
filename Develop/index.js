f51e5497d52dd958ea4de3b336ca0b42583a8a24
Seth Leininger <sethlein@gmail.com>
2023-03-28 17:49:57 -0700

    Script fixed

--- Develop/index.js
+++ Develop/index.js
@@ -1,41 +1,83 @@
 // TODO: Include packages needed for this application
 const inquirer = require(`inquirer`);
 const fs = require(`fs`);
-const gen = require(`./utils/generateMarkdown.js`)
+const generator = require(`./utils/generateMarkdown.js`)
+
 // TODO: Create an array of questions for user input
 const questions = [
     'What is the title of your project?',
     'Describe your project',
     'How do you use your project?',
-    'How do you contribute to your project?',
-    'What command should be run to insall dependancies?',
-    'What command should be run to run tests?',
+    'How did you contribute to your project?',
+    'What command should be run to install dependancies?',
     'Which license do you use?',
     'PLease enter you GitHub username?',
     'Please enter you email.'
 ];
 
-
-
-
-
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    const fileName = 'README.md';
+const fsPromises = fs.promises;
 
-    fsPromises.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
-        err ? console.log(err) : console.log('Sucess!')
-    );
+const writeToFile = (fileName, data) => {
+    //console.log(generator(data));
+    fsPromises.writeFile(fileName, generator(data));
 }
 
 const promptUser = () => {
    return inquirer
-    .prompt(questions) 
+    .prompt([
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: questions[1],
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'contributions',
+        },
+        {
+            type: 'input',
+            message: questions[4],
+            name: 'installation',
+        },
+        {
+            type: 'list',
+            message: questions[5],
+            name: 'licenses',
+            choices: ['MIT License', 'GNU GPLv3', 'GNU AGPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0'],
+        },
+        {
+            type: 'input',
+            message: questions[6],
+            name: 'GitHub',
+        },
+        {
+            type: 'input',
+            message: questions[7],
+            name: 'email',
+        },
+    ])
+    .then((data) => {
+        writeToFile("README.md", data);
+    })
 }
 
 
 // TODO: Create a function to initialize app
-function init() { }
+
+promptUser();
+//function init() { }
 
 // Function call to initialize app
-init();
+//init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,22 +1,101 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  
-
+const renderLicenseBadge = licenses => {
+  switch (licenses) {
+    case 'MIT License':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    case 'GNU GPLv3':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+    case 'GNU AGPLv3':
+      return '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)'
+    case 'GNU LGPLv3':
+      return '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)'
+    case 'Mozilla Public Lisense 2.0':
+      return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+    case 'Apache License 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    default:
+      return ' ';
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+const renderLicenseLink = licenses => { 
+  switch (licenses) {
+    case 'MIT License':
+      return 'https://opensource.org/licenses/MIT'
+    case 'GNU GPLv3':
+      return 'https://www.gnu.org/licenses/gpl-3.0'
+    case 'GNU AGPLv3':
+      return 'https://www.gnu.org/licenses/agpl-3.0'
+    case 'GNU LGPLv3':
+      return 'https://www.gnu.org/licenses/lgpl-3.0'
+    case 'Mozilla Public License 2.0':
+      return 'https://opensource.org/licenses/MPL-2.0'
+    case 'Apache License 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0'
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(licenses) { 
+  const licenseLink = renderLicenseLink(licenses);
+  if (!licenseLink) {
+    return '';
+  }
+  return `
+## License
+
+This application is covered by the [${licenses}](${licenseLink}) license.
+`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+   //const licenses = data.licenses.choices;
+   //console.log(licenses);
+   //console.log(renderLicenseSection(licenses));
+  const licenseSection = renderLicenseSection(data.licenses);
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.licenses)}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Licenses](#licenses)
+  - [Contributions](#contributions)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ${licenseSection}
+
+  ## Contributors 
+
+  ${data.contributions}
+
+  ## GitHub
+
+  GitHub User - ${data.GitHub}
+
+  ## Email
+
+  Contact the Maker at - ${data.email}
 `;
 }
 
